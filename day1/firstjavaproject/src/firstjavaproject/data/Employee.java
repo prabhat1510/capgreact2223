946a207d0e5648489bb80386dc1597b6186ca465
prabhat1510 <prabhatc@gmail.com>
2022-12-26 16:51:43 +0530

    First Java Project example

--- day1/firstjavaproject/src/firstjavaproject/data/Employee.java
+++ day1/firstjavaproject/src/firstjavaproject/data/Employee.java
@@ -5,7 +5,9 @@ public class Employee {
 	// Data members or fields or properties
 	private int empId;
 	private String empName;
-
+	//HAS-A
+	private Address address;
+	
 	// Default constructor or No Argument constructor
 	public Employee() {
 		// TODO Auto-generated constructor stub
@@ -31,10 +33,16 @@ public class Employee {
 	public void setEmpName(String empName) {
 		this.empName = empName;
 	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 	@Override
 	public String toString() {
-		return "Employee [empId=" + empId + ", empName=" + empName + "]";
+		return "Employee [empId=" + empId + ", empName=" + empName + ", address=" + address + "]";
 	}
-
+	
 
 }
